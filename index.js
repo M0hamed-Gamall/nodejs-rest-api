f7b2cd833262f507259f6a75fbaac17161d90365
M0hamed-Gamall <jimmy.mans00ur@gmail.com>
2025-08-31 20:25:25 +0300

    start the sever and connect database

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const dotenv = require('dotenv')
+dotenv.config({path: './.env'})
+const app = express()
+
+const mongoose = require('mongoose')
+const uri = `mongodb+srv://mohamedGamal:${process.env.DB_PASSWORD}@ecommerce.vxjz39s.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Ecommerce`
+mongoose.connect(uri).then(()=>{
+    console.log("mongodb connect successfully")
+})
+
+
+
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log("app is running on port ", process.env.PORT)
+})
