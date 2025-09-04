ea900742832794d42c9cce59cc91fe579ea78efb
M0hamed-Gamall <jimmy.mans00ur@gmail.com>
2025-09-05 02:58:52 +0300

    add pagination for GET /api/users

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,7 +3,10 @@ const httpStatusText = require("../utils/httpStatusText")
 const asyncWrapper = require('../middlewares/asyncWrapper')
 
 const getAllUsers = asyncWrapper( async (req, res, next) => {
-    const users = await User.find()
+    const query = req.query
+    const limit = query.limit || 5
+    const page = query.page || 1
+    const users = await User.find({}, {"__v": false}, {limit, skip: (page - 1) * limit})
     res.status(200).json({status: httpStatusText.SUCCESS, data: {users}})
 })
 
